b71f863df9c437d845ca7ba2412fb590f0469f34
Artem Derkach <artem.derkach.gs@gmail.com>
2024-09-23 16:18:34 +0300

    init

---
+++ build.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+// const Options = @import("../../build.zig").Options;
+
+const demo_name = "minimal_zgui_glfw_gl";
+const content_dir = demo_name ++ "_content/";
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    @import("system_sdk").addLibraryPathsTo(exe);
+
+    const zglfw = b.dependency("zglfw", .{
+        // .target = options.target,
+    });
+    exe.root_module.addImport("zglfw", zglfw.module("root"));
+    exe.linkLibrary(zglfw.artifact("glfw"));
+
+    const zopengl = b.dependency("zopengl", .{
+        // .target = options.target,
+    });
+    exe.root_module.addImport("zopengl", zopengl.module("root"));
+
+    const zgui = b.dependency("zgui", .{
+        // .target = options.target,
+        .backend = .glfw_opengl3,
+    });
+    exe.root_module.addImport("zgui", zgui.module("root"));
+    exe.linkLibrary(zgui.artifact("imgui"));
+
+    // const content_path = b.pathJoin(&.{ cwd_path, content_dir });
+    const install_content_step = b.addInstallDirectory(.{
+        .source_dir = b.path("./content/"),
+        .install_dir = .{ .custom = "" },
+        .install_subdir = b.pathJoin(&.{ "bin", "./content/" }),
+    });
+    exe.step.dependOn(&install_content_step.step);
+
+    b.installArtifact(exe);
+
+}
