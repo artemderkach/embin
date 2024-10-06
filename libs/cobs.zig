0ac828d5ab42ba2e4d43a10d00c248479622aafa
Artem Derkach <artemderkach@io.local>
2024-10-06 15:31:36 +0300

    done with encoding

--- libs/cobs.zig
+++ libs/cobs.zig
@@ -31,12 +31,11 @@ pub const Error = error{
     // PayloadTooLong,
 };
 
-// pub const COBSLenError = error{
-//     PayloadTooShort,
-//     PayloadTooLong,
-// };
-
 pub const source_max_len = 254;
+
+/// max diff is 255 becouse it is max number that can be
+/// represented by u8
+pub const max_diff = 255;
 
 /// finds maximum possible destination length
 /// needed to create slice that will be passed to 'cobs' function
@@ -50,13 +49,22 @@ pub fn encode(source: []u8, dest: []u8) Error![]u8 {
     if (dest.len < get_max_dest_len(source)) {
         return Error.DestinationTooShort;
     }
+    const res = encode_short(source, dest) catch |err| switch (err) {
+        Error.PayloadTooLong => return encode_long(source, dest),
+        else => return err,
+    };
 
-    return dest[0..dest.len-1];
+    return res;
 }
 
 // test "encode" {
+//     {
+//         var source: [300]u8 = undefined;
+//         var dest: [400]u8 = undefined;
+//         const res = encode(&source, &dest);
+//         try expectEqual(Error.PayloadTooLong, res);
+//     }
 //
-//     encode();
 // }
 
 /// encode short slice of bytes (<= 254)
@@ -75,128 +83,75 @@ pub fn encode_short(source: []u8, dest: []u8) Error![]u8 {
     if (dest.len < new_dest_len) {
         return Error.DestinationTooShort;
     }
-    if (source.len == 0) {
-        return Error.PayloadCannotBeEmpty;
-    }
 
-    // iterate over source backwards
+    // source iterator
     var i = source.len;
-    var dest_i = source.len + 1;
+    // destination iterator
+    var j = source.len + 1;
 
     // last byte of destination
-    dest[dest_i] = 0;
+    dest[j] = 0;
 
-    var last_zero_position = dest_i;
+    // iterate over source backwards
+    var last_zero_position = j;
     for (source) |_| {
         i -= 1;
-        dest_i -= 1;
+        j -= 1;
 
         if (source[i] == 0) {
-            const diff = last_zero_position - dest_i;
-            last_zero_position = dest_i;
-            dest[dest_i] = @as(u8, @intCast(diff));
+            // range between zero bytes
+            const diff = last_zero_position - j;
+
+            last_zero_position = j;
+            dest[j] = @as(u8, @intCast(diff));
             continue;
         }
 
-        dest[dest_i] = source[i];
+        dest[j] = source[i];
     }
     dest[0] = @as(u8, @intCast(last_zero_position));
 
     return dest[0..new_dest_len]; 
 }
 
-test "encode_short" {
-    {
-        // empty input
-        const source = [_]u8{};
-        var dest: [256]u8 = undefined;
-        const res = encode_short(&source, &dest);
-        try expectEqual(Error.PayloadCannotBeEmpty, res);
-    }
-    {
-        // empty output
-        var source: [100]u8 = undefined;
-        var dest = [_]u8{};
-        const res = encode_short(&source, &dest);
-        try expectEqual(Error.DestinationTooShort, res);
-    }
-    {
-        // destination just a bit shorter that it need to be
-        var source: [100]u8 = undefined;
-        var dest: [101]u8 = undefined;
-        const res = encode_short(&source, &dest);
-        try expectEqual(Error.DestinationTooShort, res);
-    }
-    {
-        // source just above allowed value
-        var source: [255]u8 = undefined;
-        var dest: [256]u8 = undefined;
-        const res = encode_short(&source, &dest);
-        try expectEqual(Error.PayloadTooLong, res);
-    }
-    {
-        // source too huge
-        var source: [1000]u8 = undefined;
-        var dest: [256]u8 = undefined;
-        const res = encode_short(&source, &dest);
-        try expectEqual(Error.PayloadTooLong, res);
-    }
-    {
-        // destination is just minimum needed amount
-        var source: [100]u8 = undefined;
-        var dest: [102]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqual(102, res.len);
-    }
-    {
-        // destination length is far bigger then source
-        var source: [100]u8 = undefined;
-        var dest: [1000]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqual(102, res.len);
-    }
-    {
-        // source is just at its limit
-        var source: [254]u8 = undefined;
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqual(256, res.len);
-    }
-    {
-        // simple case without any zero bytes
-        var source  = [_]u8{1, 2, 3, 4};
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqualSlices(u8, &[_]u8{5, 1, 2, 3, 4, 0}, res);
-    }
-    {
-        // simple case with single zero byte
-        var source  = [_]u8{1, 2, 0, 4, 5};
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqualSlices(u8, &[_]u8{3, 1, 2, 3, 4, 5, 0}, res);
-    }
-    {
-        // input with single zero byte
-        var source  = [_]u8{0};
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqualSlices(u8, &[_]u8{1, 1, 0}, res);
-    }
-    {
-        var source  = [_]u8{0, 0};
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqualSlices(u8, &[_]u8{1, 1, 1, 0}, res);
-    }
-    {
-        var source  = [_]u8{0, 1, 0};
-        var dest: [256]u8 = undefined;
-        const res = try encode_short(&source, &dest);
-        try expectEqualSlices(u8, &[_]u8{1, 2, 1, 1, 0}, res);
+/// encodes message without the limit and without the check for short message
+/// this is more of a classic approach for handling cobs
+/// but giving that encode is already taken, this will get _long name
+pub fn encode_long(source: []u8, dest: []u8) Error![]u8 {
+    const max_dest_len = get_max_dest_len(source);
+    if (dest.len < max_dest_len) {
+        return Error.DestinationTooShort;
     }
 
+    var zero_pos: usize = 0;
+    // destination iterator
+    var j: usize = 0;
+    for (source, 0..) |_, i| {
+        // destination is always one ahead because of overhead byte
+        j += 1;
+
+        if (j - zero_pos == max_diff) {
+            dest[zero_pos] = max_diff;
+            zero_pos = j;
+            j += 1;
+        }
+
+        if (source[i] == 0) {
+            dest[zero_pos] = @as(u8, @intCast(j - zero_pos));
+            zero_pos = j;
+            continue;
+        }
+
+        dest[j] = source[i];
+    }
+
+    j += 1;
+    dest[j] = 0;
+    dest[zero_pos] = @as(u8, @intCast(j - zero_pos)); 
+
+    return dest[0..j + 1]; 
 }
+
 
 // COBS encodeing wiht length of original message
 pub fn encode_len(source: []u8, dest: []u8) Error![]u8 {
@@ -333,6 +288,173 @@ test "get_max_dest_len" {
     var arr5: [1000]u8 = undefined;
     len = get_max_dest_len(&arr5);
     try std.testing.expectEqual(1005, len);
+}
+
+test "encode_short" {
+    {
+        // empty input
+        const source = [_]u8{};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{1, 0}, res);
+    }
+    {
+        // empty output
+        var source: [100]u8 = undefined;
+        var dest = [_]u8{};
+        const res = encode_short(&source, &dest);
+        try expectEqual(Error.DestinationTooShort, res);
+    }
+    {
+        // destination just a bit shorter that it need to be
+        var source: [100]u8 = undefined;
+        var dest: [101]u8 = undefined;
+        const res = encode_short(&source, &dest);
+        try expectEqual(Error.DestinationTooShort, res);
+    }
+    {
+        // source just above allowed value
+        var source: [255]u8 = undefined;
+        var dest: [256]u8 = undefined;
+        const res = encode_short(&source, &dest);
+        try expectEqual(Error.PayloadTooLong, res);
+    }
+    {
+        // source too huge
+        var source: [1000]u8 = undefined;
+        var dest: [256]u8 = undefined;
+        const res = encode_short(&source, &dest);
+        try expectEqual(Error.PayloadTooLong, res);
+    }
+    {
+        // destination is just minimum needed amount
+        var source: [100]u8 = undefined;
+        var dest: [102]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqual(102, res.len);
+    }
+    {
+        // destination length is far bigger then source
+        var source: [100]u8 = undefined;
+        var dest: [1000]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqual(102, res.len);
+    }
+    {
+        // source is just at its limit
+        var source: [254]u8 = undefined;
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqual(256, res.len);
+    }
+    {
+        // simple case without any zero bytes
+        var source = [_]u8{1, 2, 3, 4};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{5, 1, 2, 3, 4, 0}, res);
+    }
+    {
+        // simple case with single zero byte
+        var source = [_]u8{1, 2, 0, 4, 5};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{3, 1, 2, 3, 4, 5, 0}, res);
+    }
+    {
+        // input with single zero byte
+        var source = [_]u8{0};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{1, 1, 0}, res);
+    }
+    {
+        var source = [_]u8{0, 0};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{1, 1, 1, 0}, res);
+    }
+    {
+        var source = [_]u8{0, 1, 0};
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{1, 2, 1, 1, 0}, res);
+    }
+    {
+        // actual max size without zero bytes
+        var source = [_]u8{1} ** 254;
+        var dest: [256]u8 = undefined;
+        const res = try encode_short(&source, &dest);
+        var exp = [_]u8{1} ** 256;
+        exp[0] = 0xFF;
+        exp[0xFF] = 0;
+        try expectEqualSlices(u8, &exp, res);
+    }
+}
+
+test "encode_long" {
+    {
+        // small destination
+        var source: [100]u8 = undefined;
+        var dest: [0]u8 = undefined;
+        const res = encode_long(&source, &dest);
+        try expectEqual(Error.DestinationTooShort, res);
+    }
+    {
+        // small destination but input is large
+        var source: [800]u8 = undefined;
+        var dest: [0]u8 = undefined;
+        const res = encode_long(&source, &dest);
+        try expectEqual(Error.DestinationTooShort, res);
+    }
+    {
+        // try to encode some valid short message
+        var source = [_]u8{1, 2, 3, 4, 5};
+        var dest: [10]u8 = undefined;
+        const res = try encode_long(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{6, 1, 2, 3, 4, 5, 0}, res);
+    }
+    {
+        // try to encode some valid short message with some zero bytes
+        var source = [_]u8{1, 0, 2, 3, 0, 4, 5};
+        var dest: [10]u8 = undefined;
+        const res = try encode_long(&source, &dest);
+        try expectEqualSlices(u8, &[_]u8{2, 1, 3, 2, 3, 3, 4, 5, 0}, res);
+    }
+    {
+        // try to encode some valid short message with some zero bytes
+        var source = [_]u8{1} ** 256;
+        var dest: [300]u8 = undefined;
+        const res = try encode_long(&source, &dest);
+
+        var exp = [_]u8{1} ** 259;
+        exp[0] = 0xFF;
+        exp[255] = 3;
+        exp[258] = 0;
+        try expectEqualSlices(u8, &exp, res);
+    }
+    {
+        // lot of data
+        var source = [_]u8{1} ** 1000;
+        source[200] = 0;
+        source[400] = 0;
+        var dest: [1100]u8 = undefined;
+        const res = try encode_long(&source, &dest);
+
+        var exp = [_]u8{1} ** 1004;
+        exp[0] = 201;
+        exp[201] = 200;
+        exp[401] = 255;
+        exp[656] = 255;
+        exp[911] = 92;
+        exp[1003] = 0;
+        try expectEqual(exp[0], dest[0]);
+        try expectEqual(exp[201], dest[201]);
+        try expectEqual(exp[401], dest[401]);
+        try expectEqual(exp[656], dest[656]);
+        try expectEqual(exp[911], dest[911]);
+        try expectEqualSlices(u8, &exp, res);
+    }
 }
 
 
