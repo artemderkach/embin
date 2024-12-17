01065c294f27ea2eb71bbcb0155b78400c5e987d
artemderkach <artem.derkach.gs@gmail.com>
2024-12-17 22:53:50 +0200

    draw sin

--- cmd/listen.zig
+++ cmd/listen.zig
@@ -91,7 +91,43 @@ pub fn Execute(_: *Self) !void {
     std.debug.print("{any}", .{list2.items});
     // list.
 
+    const startTime: i64 = std.time.milliTimestamp();
+    var fromStart: i64 = 0;
+    var prevTime: i64 = std.time.milliTimestamp();
+
+    var samplesX: [100]f64 = undefined;
+    var samples: [100]f64 = undefined;
+
     while (!window.shouldClose() and window.getKey(.escape) != .press) {
+        fromStart = std.time.milliTimestamp() - startTime;
+
+        std.debug.print("{any}\n", .{prevTime - std.time.milliTimestamp()});
+        prevTime = std.time.milliTimestamp();
+
+        if (isNextFrame()) {
+            for (samplesX, 0..) |_, i| {
+                samplesX[i] = @as(f64, @floatFromInt(i));
+            }
+
+            for (samples, 0..) |_, i| {
+                samples[i] = @sin(@as(f64, @floatFromInt(i)) * @as(f64, 0.2) + @as(f64, @floatFromInt(fromStart)) * @as(f64, 1.5));
+            }
+        }
+
+        const first = list.pop();
+        try list.insert(0, first);
+
+        const first2 = list2.pop();
+        try list2.insert(0, first2);
+
+        // std.debug.print("{any}", .{list.items});
+        // std.debug.print("{any}\n", .{list2.items});
+        // std.debug.print("{any}\n", .{isNextFrame()});
+
+        //
+        // std.debug.print("loop...\n", .{});
+        // std.time.sleep(std.time.ns_per_s);
+
         glfw.pollEvents();
 
         gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0, 0, 0, 1.0 });
@@ -116,16 +152,23 @@ pub fn Execute(_: *Self) !void {
             zgui.plot.setupAxisLimits(.x1, .{ .min = 0, .max = 5 });
             zgui.plot.setupLegend(.{ .south = true, .west = true }, .{});
             zgui.plot.setupFinish();
-            zgui.plot.plotLineValues("y data", i32, .{
-                .v = list.items,
-                // .v = &.{1, 2, 3, 4}
+
+            // zgui.plot.plotLineValues("y data", i32, .{
+            //     .v = list.items,
+            //     // .v = &.{1, 2, 3, 4}
+            // });
+            zgui.plot.plotLine("xy data", f64, .{
+                .xv = &samplesX,
+                .yv = &samples,
             });
-            zgui.plot.plotLine("xy data", f32, .{
-                // .xv = &.{ 0.1, 0.2, 0.5, 2.5 },
-                // .yv = &.{ 0.1, 0.3, 0.5, 0.9 },
-                .xv = list2.items,
-                .yv = list2.items,
-            });
+
+            // .{
+            // .xv = &.{ 0.1, 0.2, 0.5, 2.5 },
+            // .yv = &.{ 0.1, 0.3, 0.5, 0.9 },
+            // .xv = list2.items,
+            // .yv = list2.items,
+            // }
+            // );
             // zgui.plot.plotScatter("test scatter", f32, .{
             //     // .xv = &.{1.5, 1.9, 2.2},
             //     // .yv = &.{1.5, 1.9, 2.2},
@@ -141,4 +184,33 @@ pub fn Execute(_: *Self) !void {
 
         window.swapBuffers();
     }
+}
+
+var time: i64 = undefined;
+const frameDuration: i64 = 1000; // > 60 fps
+
+fn isNextFrame() bool {
+    if (time == undefined) {
+        time = std.time.milliTimestamp();
+    }
+    const currTime = std.time.milliTimestamp();
+    const diff = currTime - time;
+    const remaider = @rem(diff, frameDuration);
+
+    if (diff > frameDuration) {
+        time = currTime - remaider;
+        return true;
+    }
+
+    return false;
+}
+
+var nextCounter: f64 = undefined;
+fn next() f64 {
+    if (nextCounter == undefined) {
+        nextCounter = @as(f64, @floatFromInt(std.time.milliTimestamp()));
+    }
+    nextCounter += 1.0;
+
+    return nextCounter;
 }
